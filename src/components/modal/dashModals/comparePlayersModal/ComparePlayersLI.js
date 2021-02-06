b2526624d8677725998d09278969c86be1b3cc5b
TreHein <treheinguy@gmail.com>
2021-02-06 15:40:19 -0600

    colorBox motion div transitions wired and firing

--- src/components/modal/dashModals/comparePlayersModal/ComparePlayersLI.js
+++ src/components/modal/dashModals/comparePlayersModal/ComparePlayersLI.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import ClubLogoPicker from '../../../../utils/ClubLogoPicker'
+import { motion } from 'framer-motion'
+
+const variants = {
+    open: { opacity: 1, x: 0, transition: {duration: .75} },
+    closed: { opacity: 0, x: 30, transition: {duration: .75} }
+}
 
 const ComparePlayersLI = ({ player, onPlayers, setOnPlayers, index, color }) => {
     console.log(color)
@@ -31,7 +37,14 @@ return (
                     <h5>
                         {player.totalPTS}
                     </h5>
-                        <div className="colorBox" style={{background: `${color}`}} />
+                    <motion.div 
+                        className="colorBox" 
+                        style={{background: `${color}`}} 
+                        opacity="0"
+                        variants={variants} 
+                        initial="closed"
+                        animate={ onPlayers[index] ? "open" : "closed" } 
+                    />
                 </div>
             </div>
         </li>
