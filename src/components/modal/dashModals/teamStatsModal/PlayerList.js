17f701aa89cfe7d96f5898cd22930d56198914c7
TreHein <treheinguy@gmail.com>
2021-02-28 09:08:18 -0600

    TeamStatsModal playerList header complete. todo - add motion li components

--- src/components/modal/dashModals/teamStatsModal/PlayerList.js
+++ src/components/modal/dashModals/teamStatsModal/PlayerList.js
@@ -1,10 +1,30 @@
 import React from 'react'
 
-const PlayerList = (players) => {
-    console.log(players)
-    return (
-        <div>
+const PlayerList = ({ players, onSlice }) => {
 
+    return (
+        <div className="playerListContainer">
+            <div className="playerListHeader">
+                <div className="headerBox">
+                    <div className="headerPlayerName">
+                        <h3>
+                            Player
+                        </h3>
+                    </div>
+                </div>
+                <div className="headerBox">
+                    <div className="headerStatBox">
+                        <h3>
+                            Position
+                        </h3>
+                    </div>
+                    <div className="headerStatBox">
+                        <h3>
+                            Goals
+                        </h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
